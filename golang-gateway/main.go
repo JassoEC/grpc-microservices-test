7903325d3049849ea3b95b7da2ede180545ba590
Emanuel Campos Jasso <34528183+JassoEC@users.noreply.github.com>
2024-09-09 18:04:19 -0600

    project start

---
+++ golang-gateway/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	app := gin.Default()
+
+	app := gin.Default()
+	app.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+					"message": "pong",
+			})
+	})
+	app.Run()
+
+}
